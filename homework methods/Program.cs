f5f45b8161bcf988128171eee08cabb6d7eed5ae
asmannn18 <asimangj@code.edu.az>
2022-11-19 17:00:44 +0400

    added task

---
+++ homework methods/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace homework_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator(2023556, 5, "*");
+        }
+        static void calculator(int num1,int num2,string operation)
+        {
+            int sum = 0;
+            switch (operation)
+            {
+                case "+":
+                    sum = num1 + num2;
+                    break;
+                case "-":
+                    sum = num1 - num2;
+                    break;
+                case "*":
+                    sum = num1 * num2;
+                    break;
+                case "/":
+                    sum = num1 / num2;
+                    break;
+            }
+            Console.WriteLine( sum);
+        }
+    }
+}
